156f369a1d1975c7671cdf13a7fa0d4d3b21f41d
unknown <ck2860@rit.edu>
2019-08-29 19:21:01 -0400

    My first commit

---
+++ src/activities/session02/Expressions.java
@@ -0,0 +1,12 @@
+package activities.session02;
+
+public class Expressions {
+    public static void main(String[] args) {
+        int x = 2;
+        int y = 8 / 2 * 6;
+        int z = x + y;
+        System.out.println(y);
+        System.out.println(x);
+        System.out.println(z);
+    }
+}
