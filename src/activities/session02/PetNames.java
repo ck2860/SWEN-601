b977e3786a3075ad56816ef99c05fb97f634fe0d
unknown <ck2860@rit.edu>
2019-08-29 21:01:48 -0400

    My first commit

---
+++ src/activities/session02/PetNames.java
@@ -0,0 +1,17 @@
+package activities.session02;
+
+public class PetNames {
+    public static void sayPetName(){
+        System.out.println("M");
+        System.out.println("i");
+        System.out.println("l");
+        System.out.println("l");
+        System.out.println("i");
+        System.out.println("e");
+    }
+    public static void main(String[] args) {
+        sayPetName();
+        sayPetName();
+        sayPetName();
+    }
+}
