3143e41cc878d6d50072478cb8653003811c8470
unknown <ck2860@rit.edu>
2019-08-29 22:27:19 -0400

    My third commit

--- src/activities/session02/Calculator.java
+++ src/activities/session02/Calculator.java
@@ -1,10 +1,28 @@
 package activities.session02;
 
-public class calculator {
+public class Calculator {
     public static void calculate(float x, float y) {
-        System.out.println(" x = "+ x + " y = " + y);
+        System.out.println("x = "+ x + " y = " + y);
+        System.out.println("x + y = " + (x + y));
+        System.out.println("x * y = " + (x * y));
+        System.out.println("x - y = " + (x - y));
+        System.out.println("x / y = " + (x / y));
+
     }
+
+    public static float convert(float pounds) {
+        System.out.println("pounds: " + pounds);
+        System.out.println("kilos: " + (pounds/2.2));
+        return (pounds);
+    }
+
     public static void main(String[] args) {
         calculate(4,6 );
+        calculate(5,3);
+        calculate(8, 9);
+        float result = convert((float)186.2);
+        float result1 = convert((float)207);
+        System.out.println("total: " + ((result+result1)/2.2));
+
     }
 }
