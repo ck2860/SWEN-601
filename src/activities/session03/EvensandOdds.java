a14125151aaf68a18cd1f9ba3b304aabe8ce605f
unknown <ck2860@rit.edu>
2019-09-03 11:43:06 -0400

    Second commit

--- src/activities/session03/EvensandOdds.java
+++ src/activities/session03/EvensandOdds.java
@@ -5,11 +5,13 @@ import java.util.Scanner;
 public class EvensandOdds {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a number");
+        System.out.print("Enter a number ");
 
         int i = scanner.nextInt();
         if (i % 2 == 0) {
-            System.out.println("Number is is even.");
+            System.out.println("The Number is even.");
+        }else {
+            System.out.println("The number is odd");
         }
     }
 }
